fd771c5834a98e05af3b745c6244983c7977ee2c
Gus Knowlden <gus@heavyset.io>
2020-12-03 09:38:12 -0500

    2020-day3: refactoring loop

--- 2020/cmd/day3/main.go
+++ 2020/cmd/day3/main.go
@@ -9,48 +9,38 @@ import (
 
 const tree = '#'
 
-func treesInPath(llen, xinc, yinc int, slope []string) int {
+func treesInPath(xinc, yinc int, slope []string) int {
+	llen := len(slope[0])
 	xpos := 0
-	ypos := 0
 	ctr := 0
-	for _, line := range slope {
-		ypos++
-		if ypos == yinc {
-			xpos = (xpos + xinc) % llen
-			if line[xpos] == tree {
-				ctr++
-			}
-			ypos = 0
+	for ypos := yinc; ypos < len(slope); ypos += yinc {
+		xpos = (xpos + xinc) % llen
+		if slope[ypos][xpos] == tree {
+			ctr++
 		}
 	}
 	return ctr
 }
 
 func main() {
-	llen := -1
 	lines := make([]string, 0)
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		line := scanner.Text()
-		if llen == -1 {
-			llen = len(line)
-			continue
-		}
-		lines = append(lines, line)
+		lines = append(lines, scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("error reading input: %v", err)
 	}
 
 	// part 1
-	fmt.Printf("part 1: trees encountered: %d", treesInPath(llen, 3, 1, lines))
+	fmt.Printf("part 1: %d\n", treesInPath(3, 1, lines))
 
 	// part 2
-	p1 := treesInPath(llen, 1, 1, lines)
-	p2 := treesInPath(llen, 3, 1, lines)
-	p3 := treesInPath(llen, 5, 1, lines)
-	p4 := treesInPath(llen, 7, 1, lines)
-	p5 := treesInPath(llen, 1, 2, lines)
+	p1 := treesInPath(1, 1, lines)
+	p2 := treesInPath(3, 1, lines)
+	p3 := treesInPath(5, 1, lines)
+	p4 := treesInPath(7, 1, lines)
+	p5 := treesInPath(1, 2, lines)
 
-	fmt.Printf("part 2: [%d %d %d %d %d]: %d", p1, p2, p3, p4, p5, p1*p2*p3*p4*p5)
+	fmt.Printf("part 2: [%d %d %d %d %d]: %d\n", p1, p2, p3, p4, p5, p1*p2*p3*p4*p5)
 }
