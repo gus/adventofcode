34e09040088d2e0b8baa0d9167a4b8d42aee5993
Gus Knowlden <gus@heavyset.io>
2020-12-10 12:38:30 -0500

    2020-day10: really cleaned up the algorithm for part2

--- 2020/cmd/day10/main.go
+++ 2020/cmd/day10/main.go
@@ -10,35 +10,22 @@ import (
 	"github.com/gus/adventofcode/2020/internal/slices"
 )
 
-func diffHist(a slices.Int64) maps.Int64 {
-	last := int64(a[0])
+func diffHistProduct(a slices.Int64) int64 {
+	last := a[0]
 	hist := maps.Int64{}
 	for _, j := range a[1:] {
 		hist.Incr(j-last, 1)
 		last = j
 	}
-	return hist
+	return hist.Get(1, 0) * hist.Get(3, 0)
 }
 
 func pathCount(a slices.Int64) int64 {
-	edgeIdx := map[int64]slices.Int64{}
-
-	// graph adapter-edges for each adapter
-	for i, j := range a {
-		edgeIdx[j] = slices.Int64{}
-		for ei := i + 1; ei < len(a) && a[ei]-j <= 3; ei++ {
-			edgeIdx[j] = edgeIdx[j].Append(a[ei])
-		}
+	pathCtrIdx := maps.Int64{0: 1}
+	for _, j := range a[1:] {
+		pathCtrIdx.Incr(j, pathCtrIdx.Get(j-1, 0)+pathCtrIdx.Get(j-2, 0)+pathCtrIdx.Get(j-3, 0))
 	}
-
-	// memoize path count per adapter, reverse sum
-	pathCtrIdx := maps.Int64{a.Get(-1): 1}
-	for i := a.LastIndex() - 1; i >= 0; i-- {
-		for _, e := range edgeIdx[a[i]] {
-			pathCtrIdx.Incr(a[i], pathCtrIdx[e])
-		}
-	}
-	return pathCtrIdx[0]
+	return pathCtrIdx.Get(a.Get(-1), 0)
 }
 
 func main() {
@@ -53,8 +40,6 @@ func main() {
 
 	adapters = adapters.Sort().Append(adapters.Get(-1) + 3)
 
-	part1 := diffHist(adapters)
-	fmt.Printf("%v\n", part1)
-	fmt.Printf("part 1: %d\n", part1[1]*part1[3])
+	fmt.Printf("part 1: %d\n", diffHistProduct(adapters))
 	fmt.Printf("part 2: %d\n", pathCount(adapters))
 }
