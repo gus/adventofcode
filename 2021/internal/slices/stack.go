bf34f46f52ec2330b2d05762d57cdc0354009ab7
Gus Knowlden <gus@heavyset.io>
2021-12-11 12:55:45 -0500

    2021: slices.Stack type is now just a slice of T

--- 2021/internal/slices/stack.go
+++ 2021/internal/slices/stack.go
@@ -1,33 +1,31 @@
 package slices
 
-type Stack[T any] struct {
-	s []T
-}
+type Stack[T any] []T
 
-func NewStack[T any](elems ...T) *Stack[T] {
-	return &Stack[T]{elems}
+func NewStack[T any](elems ...T) Stack[T] {
+	return Stack[T](elems)
 }
 
 // Slice returns the stack as a slice, with elements in reverse order for easier
 // consumption.
-func (s *Stack[T]) Slice() []T {
-	rs := make([]T, len(s.s))
-	for i := 0; i < len(s.s); i++ {
-		rs[len(s.s)-i-1] = s.s[i]
+func (s Stack[T]) Slice() []T {
+	rs := make([]T, len(s))
+	for i := 0; i < len(s); i++ {
+		rs[len(s)-i-1] = s[i]
 	}
 	return rs
 }
 
 func (s *Stack[T]) Push(v T) {
-	s.s = append(s.s, v)
+	*s = append(*s, v)
 }
 
 func (s *Stack[T]) Pop() (T, bool) {
-	if len(s.s) == 0 {
+	if len(*s) == 0 {
 		var zero T
 		return zero, false
 	}
-	elem := s.s[len(s.s)-1]
-	s.s = s.s[:len(s.s)-1]
+	elem := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
 	return elem, true
 }
