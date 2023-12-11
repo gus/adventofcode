6944dc17ea24fdc04c39d0c0c147c85087be0f76
Gus Knowlden <gus@heavyset.io>
2023-12-11 17:52:40 -0500

    2023: converts 2023 days 1-7 into a single haskell package

---
+++ aoc-haskell/days/src/2023/01.hs
@@ -0,0 +1,37 @@
+module Main (
+  main
+) where
+
+import Data.Char (isDigit, digitToInt)
+import Data.List (isPrefixOf, findIndex)
+import Data.Maybe (fromJust, isJust)
+
+main :: IO ()
+main = do
+  contents <- getContents
+  print (calibrateInputs (lines contents))
+
+whichDigit :: String -> Maybe Int
+whichDigit s = findIndex (`isPrefixOf` s) ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+wordsToDigits :: String -> [Int]
+wordsToDigits [] = []
+wordsToDigits s@(x:xs)
+  | isJust idx = fromJust idx:wordsToDigits xs
+  | isDigit x = digitToInt x:wordsToDigits xs
+  | otherwise = wordsToDigits xs
+    where idx = whichDigit s
+
+calibrateInput :: [Int] -> Int
+calibrateInput [] = 0
+calibrateInput xs = head xs * 10 + last xs
+
+calibrateInputs :: [String] -> [Int]
+calibrateInputs xs = [
+    sum [calibrateInput (map digitToInt (filter isDigit s)) | s <- xs], -- part 1
+    sum [calibrateInput (wordsToDigits s) | s <- xs] -- part 2
+  ]
+
+-- example 1: [142, 142]
+-- example 2: [209, 281]
+-- solution: [54450, 54265]
---
+++ aoc-haskell/days/src/2023/04.hs
@@ -0,0 +1,31 @@
+module Main (main) where
+
+import Data.List.Utils (split)
+
+main :: IO ()
+main = do
+  contents <- getContents
+  print (solve (lines contents))
+
+parseCards :: [String] -> [Int]
+parseCards [] = []
+parseCards (s : ss) =
+  let wn = map words $ split " | " $ last (split ": " s)
+   in length (filter (`elem` head wn) (last wn)) : parseCards ss
+
+countCopies :: Int -> Int -> [Int] -> Int
+countCopies 0 acc _ = acc
+countCopies _ acc [] = acc
+-- could try using Data.Map here next time
+countCopies i acc (n : ns) = countCopies (i - 1) (acc + 1 + countCopies n 0 ns) ns
+
+solve :: [String] -> [Int]
+solve ss =
+  let cards = parseCards ss
+   in [ sum
+          [if n == 0 then 0 else 2 ^ (n - 1) | n <- cards], -- part 1
+        countCopies (length cards) 0 cards -- part 2
+      ]
+
+-- example: [13, 30]
+-- part 1: [20117, 13768818]
